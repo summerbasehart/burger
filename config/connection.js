c29174e35e85734fdb82ed403eaad745c7caad37
unknown <summerbasehart@gmail.com>
2019-05-21 20:29:34 -0700

    connection

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,18 @@
 const mysql2 = require ('mysql')
 
 var connection = mysql.createConnection({
-    host     : localhost,
-    user     : root,
-    password : 'Phil!1993',
-  });
+  host: 'localhost',
+  port: '3306',
+  user: 'root',
+  password: 'Phil!1993',
+  database: 'burgers_db'
+});
    
-  connection.connect(function(err) {
-    if (err) {
-      console.error('error connecting: ' + err.stack);
-      return;
-    }
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
    
-    console.log('connected as id ' + connection.threadId);
-  });
+  console.log('connected as id ' + connection.threadId);
+});
